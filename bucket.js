3734ef5378606ccabb067bfb5be4493d5d84e75d
Ramarti <raul.mf86@gmail.com>
2020-02-11 13:08:59 +0100

    updated dependencies

--- bucket.js
+++ bucket.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports["default"] = void 0;
 
 var _cliSharedUtils = require("@vue/cli-shared-utils");
 
 var _mimeTypes = _interopRequireDefault(require("mime-types"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
@@ -236,12 +236,13 @@ function () {
 
       console.log(uploadFileKey);
 
-      if (uploadOptions.gzip || uploadFileKey.endsWith('gz')) {
+      if (uploadOptions.gzip || uploadFileKey.endsWith('.gz')) {
         uploadParams.ContentEncoding = 'gzip';
-      } else if (uploadFileKey.endsWith('br')) {
+      } else if (uploadFileKey.endsWith('.br')) {
         uploadParams.ContentEncoding = 'br';
       }
 
+      console.log(uploadParams);
       return this.connection.upload(uploadParams, {
         partSize: 5 * 1024 * 1024,
         queueSize: 4
@@ -251,8 +252,8 @@ function () {
     key: "contentTypeFor",
     value: function contentTypeFor(filename) {
       var lookup = filename.replace(/\.((br)|(gz))/, '');
-      console.log(_mimeTypes.default.lookup(lookup));
-      return _mimeTypes.default.lookup(lookup) || 'application/octet-stream';
+      console.log(_mimeTypes["default"].lookup(lookup));
+      return _mimeTypes["default"].lookup(lookup) || 'application/octet-stream';
     }
   }]);
 
@@ -260,4 +261,4 @@ function () {
 }();
 
 var _default = Bucket;
-exports.default = _default;
+exports["default"] = _default;
